a6e1d70acaa6051fe6e598b99660d720e83ae383
WiIIiam <rongdo54711@gmail.com>
2021-04-10 09:12:55 +0700

    Update Login and register activity

---
+++ app/src/main/java/com/william/calendar/Login/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.william.calendar.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.william.calendar.Daily_Training;
+import com.william.calendar.MainActivity;
+import com.william.calendar.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+    }
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+    }
+
+
+
+    public void LoginClick(View view) {
+        startActivity(new Intent(this,MainActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+        Log.d("DEBUG","Login Succesful" );
+    }
+
+
+}
